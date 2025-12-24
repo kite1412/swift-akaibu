2ea2f627d32bbd3c20fa67f3784bdaaff4570a91
kite1412 <frozradit@gmail.com>
2025-12-24 21:03:55 +0700

    Initial Commit

---
+++ Akaibu/AkaibuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkaibuApp.swift
+//  Akaibu
+//
+//  Created by kite1412 on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AkaibuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
